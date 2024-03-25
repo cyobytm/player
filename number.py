9ec30a402e4964ca4b0b66f02caff70b94896a35
Andrei <cyobytm@gmail.com>
2024-03-25 22:20:45 +0200

    now we try to guess one random number with input help

--- number.py
+++ number.py
@@ -1,5 +1,10 @@
 
 # importing the random module
 import random
-numere=[random.randint(0,100) for i in range(6)]
-print(numere)
+numere=[random.randint(0,100)]
+userx=int(input("ghiciti numarul, introduce numar \n  "))
+print("numarul random este ", numere)
+if userx == numere:
+    print("Ai ghicit")
+else:
+  print("Nu ai ghicit")
