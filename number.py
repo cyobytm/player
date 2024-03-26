3b2f36b333219cb27b9808ba8c50931aa39638a2
Andrei <cyobytm@gmail.com>
2024-03-26 21:26:13 +0200

    add to verify unic elements in list

--- number.py
+++ number.py
@@ -18,3 +18,10 @@ else:
     print("toate numerele se afla inre 1 si 49")
 print(all([nr in range(1,50) for nr in numere]))
 print(all([nr in range(1,50) for nr in numere_jucate]))
+# verificam daca elementele din nr random sunt unice si nu se repeta cu ajutrul set de numere
+print(len(numere) == len(set(numere)))
+print("aici am printat setul de numere random", set(numere))
+
+# verificam daca nr sunt unice din nr input
+print(len(numere_jucate) == len(set(numere_jucate)))
+print("aici am printat setul de numere random", set(numere_jucate))
