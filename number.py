5d05f10ca8accd4084df0604e71c90a8fac837a6
Andrei <cyobytm@gmail.com>
2024-03-26 13:41:30 +0200

    here we add a categorii for payed number

--- number.py
+++ number.py
@@ -7,4 +7,7 @@ numere_comune=[nr for nr in numere_jucate if nr in numere]
 print("numerele random", numere)
 print("numrele jucate", numere_jucate)
 print("numerele comune", numere_comune)
-
+categorii={6:1, 5:2, 4:3, 3:4}
+print("categoria : ", categorii.get (len(numere_comune), 0))
+##sau varianta 2
+print("categoria : ", categorii[len(numere_comune)] if len(numere_comune) in categorii else  0)
