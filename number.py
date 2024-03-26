7eb3ab9090bee7da07abe13de43db20bba7244a9
Andrei <cyobytm@gmail.com>
2024-03-26 13:20:26 +0200

    refactoring of list using list comprehention

--- number.py
+++ number.py
@@ -1,17 +1,10 @@
 
 # importing the random module
 import random
-numere=[random.randint(1,49) for i in range(6)]
-numere_jucate=[]
-
-for nr in range(6):   
-    numar_jucat=int(input("ghiciti numarul, introduce numar \n  "))
-    numere_jucate.append(numar_jucat)
-numere_comune=[]
-for nr in numere_jucate:
-    if nr in numere:
-       numere_comune.append(nr)
+numere=[random.randint(1,49) for nr in range(6)]
+numere_jucate=[int(input("ghiciti numarul, introduce numar \n  ")) for nr in range(6)]
+numere_comune=[nr for nr in numere_jucate if nr in numere]
 print("numerele random", numere)
 print("numrele jucate", numere_jucate)
 print("numerele comune", numere_comune)
-# for nr in numere_jucate:
+
