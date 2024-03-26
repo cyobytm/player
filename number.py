7887fd58d0e2003c1adbf67058cd2309af7e708c
Andrei <cyobytm@gmail.com>
2024-03-26 13:07:09 +0200

    return common number at 6 random numbers

--- number.py
+++ number.py
@@ -2,12 +2,16 @@
 # importing the random module
 import random
 numere=[random.randint(1,49) for i in range(6)]
-print(numere)
-numar_jucat=int(input("ghiciti numarul, introduce numar \n  "))
-for nr in numere:   
-    if numar_jucat == nr:
-        print("Numarul extras se afla in numerele jucate")
-        break
-else:
-       print("Numarul extras NU se afla in numerele jucate") 
-print(any ([numar_jucat == nr for nr in numere]))
+numere_jucate=[]
+
+for nr in range(6):   
+    numar_jucat=int(input("ghiciti numarul, introduce numar \n  "))
+    numere_jucate.append(numar_jucat)
+numere_comune=[]
+for nr in numere_jucate:
+    if nr in numere:
+       numere_comune.append(nr)
+print("numerele random", numere)
+print("numrele jucate", numere_jucate)
+print("numerele comune", numere_comune)
+# for nr in numere_jucate:
